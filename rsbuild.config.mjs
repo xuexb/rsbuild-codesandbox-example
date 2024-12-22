e19c09bc0ff080867e5dd15b7c2cabc275b1a008
xuexb <fe.xiaowu@gmail.com>
2024-12-22 18:17:00 +0800

    feat: 添加复现 bug

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from "@rsbuild/plugin-sass";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginSass({
+      sassLoaderOptions: {
+        additionalData: [
+          `@use "./_var.scss" as *;`,
+        ].join(""),
+      },
+    }),
+  ],
 });
